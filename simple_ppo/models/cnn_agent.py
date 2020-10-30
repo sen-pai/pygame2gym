c69913768b10c33ee50ae74adbf7eb8005acebdb
Sharan Pai <57347192+sen-pai@users.noreply.github.com>
2020-10-30 10:02:41 +0530

    fixed pos

--- simple_ppo/models/cnn_agent.py
+++ simple_ppo/models/cnn_agent.py
@@ -5,34 +5,9 @@ from torch.distributions import Categorical
 import numpy as np
 
 
-class Mish(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-        return x * (torch.tanh(F.softplus(x)))
-
-
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
-
-
-#
-# # define policy network
-# class cnn_policy_net(nn.Module):
-#     def __init__(
-#         self, nS, nH, nA
-#     ):  # nS: state space size, nH: n. of neurons in hidden layer, nA: size action space
-#         super(policy_net, self).__init__()
-#         self.h = nn.Linear(nS, nH)
-#         self.out = nn.Linear(nH, nA)
-#
-#     # define forward pass with one hidden layer with ReLU activation and sofmax after output layer
-#     def forward(self, x):
-#         x = F.relu(self.h(x))
-#         x = F.softmax(self.out(x), dim=1)
-#         return x
 
 
 class Print(nn.Module):
@@ -45,12 +20,12 @@ class Print(nn.Module):
 
 
 class cnn_policy_net(nn.Module):
-    def __init__(self, state_size, hidden_size, n_actions):
+    def __init__(self, n_channels, n_actions):
         super(cnn_policy_net, self).__init__()
 
         # For 128x128 input
         self.main_chunk = nn.Sequential(
-            nn.Conv2d(num_inputs, 32, kernel_size=5, stride=2),
+            nn.Conv2d(n_channels, 32, kernel_size=5, stride=2),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 32, kernel_size=5, stride=2),
@@ -63,9 +38,7 @@ class cnn_policy_net(nn.Module):
             Flatten(),
         )
 
-        self.remaining = nn.Sequential(
-            Mish(), nn.Linear(hidden_size, n_actions), nn.Softmax(dim=-1),
-        )
+        self.remaining = nn.Sequential(nn.Linear(hidden_size, n_actions), nn.Softmax(dim=-1),)
 
     def forward(self, obs):
         return self.remaining(self.main_chunk(obs))
@@ -86,7 +59,26 @@ class cnn_policy_net(nn.Module):
         entropy = dist.entropy()
         return entropy, log_prob
 
-    def forward(self, x):
-        x = self.main(x)
 
-        return x
+class cnn_value_net(nn.Module):
+    def __init__(self, n_channels, n_actions):
+        super(cnn_value_net, self).__init__()
+
+        # For 128x128 input
+        self.main_chunk = nn.Sequential(
+            nn.Conv2d(num_inputs, 32, kernel_size=5, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=5, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=4, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            Print(),
+            Flatten(),
+            nn.Linear(hidden_size, n_actions),
+        )
+
+    def forward(self, obs):
+        return self.main_chunk(obs)
--- simple_ppo/ppo_classic_gym.py
+++ simple_ppo/ppo_classic_gym.py
@@ -1,7 +1,5 @@
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Categorical
 from torch.utils import tensorboard
 
 import argparse
